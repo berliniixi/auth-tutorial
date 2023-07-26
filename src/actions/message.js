d62f038157f4f8c386655547333e0e00fad1b0c9
steliosT <steliosevoix@gmail.com>
2023-07-26 12:44:21 +0300

    auth actions creator

--- src/actions/message.js
+++ src/actions/message.js
@@ -1,3 +1,5 @@
+//defining two action creator functions for Redux. These action creators create actions that can be dispatched to update the Redux store
+
 import { SET_MESSAGE, CLEAR_MESSAGE } from "./types";
 
 export const getMessage = (message) => ({
