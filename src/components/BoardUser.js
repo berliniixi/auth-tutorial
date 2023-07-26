697583dec73adf8bf79d49dde8ed392b72bdbc91
steliosT <steliosevoix@gmail.com>
2023-07-26 16:18:09 +0300

    create pages for accessing resources and role-based pages

---
+++ src/components/BoardUser.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import userService from "../services/user.service";
+
+function BoardUser() {
+  const [content, setContent] = useState("");
+
+  useEffect(() => {
+    userService.getUserBoard().then(
+      (response) => {
+        setContent(response.data);
+      },
+      (error) => {
+        const _content =
+          (error.response &&
+            error.response.data &&
+            error.response.data.message) ||
+          error.message ||
+          error.toString();
+        setContent(_content);
+      }
+    );
+  }, []);
+  return (
+    <div className="container">
+      <header className="jumbotron">
+        <h3>{content}</h3>
+      </header>
+    </div>
+  );
+}
+
+export default BoardUser;
